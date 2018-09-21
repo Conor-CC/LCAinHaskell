11db9868de8b3412f8d525ad6be3d9fd269961ae
cleryc <cleryc@tcd.ie>
2018-09-21 20:56:31 +0100

    Added in Binary Search Tree functionality

--- test/Spec.hs
+++ test/Spec.hs
@@ -10,3 +10,7 @@ main = hspec $ do
     it "First given parameter should be returned." $ do
       (returnTypedParam 3) `shouldBe` (3 :: Int)
       (returnTypedParam "Sample Text") `shouldBe` ("Sample Text" :: String)
+
+  describe "returnPeriklesQuote" $ do
+    it "Perikles Quote should be returned: Judging happiness to be the fruit of freedom and freedom that of valour, we shall never shy from the dangers of war." $ do
+      returnPeriklesQuote `shouldBe` ("Judging happiness to be the fruit of freedom and freedom that of valour, we shall never shy from the dangers of war." :: String)
